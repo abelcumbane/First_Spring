05ffcaccbb480accf2efdd8dac789f8c2ec7bb7a
Abel Cumbane <abelrosac@gmail.com>
2025-02-26 08:58:45 +0100

    first commit

---
+++ src/main/java/com/springfremeworkproject/App.java
@@ -0,0 +1,23 @@
+package com.springfremeworkproject;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		//Inversão de Controle (IoC) – O Spring gerencia os objetos da aplicação, reduzindo o acoplamento
+		//IoC container - this is where the bean are located
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		
+		/*
+		 * We do not get a NULL exception because the spring container
+		 * does not instantiation under the hood
+		 * */
+		Student s = (Student) context.getBean("student_bean");
+		s.sayHello();
+	}
+
+}
---
+++ src/main/java/com/springfremeworkproject/Student.java
@@ -0,0 +1,10 @@
+package com.springfremeworkproject;
+
+public class Student {
+	
+	public void sayHello() {
+		
+		System.out.println("Bom dia mundo de Spring Fremework");
+	}
+
+}
